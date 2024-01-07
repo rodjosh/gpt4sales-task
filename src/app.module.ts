ebe8740e3c11fe41492c00671a3158d74c644621
Joshua Rodriguez <joshua.david.rodriguez.medrano@gmail.com>
2024-01-07 21:19:15 +0100

    Add sequelize module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import { SequelizeModule } from '@nestjs/sequelize';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: 'database.sqlite',
+      models: [],
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
